ec667756e40e49d75d1dcc861e7fca401f39ae45
Lenara Anafieva <lenarik.ak@gmail.com>
2019-02-10 23:17:24 +0200

    Implemented events loading per specific client_id on EventsList screen

---
+++ components/ClientMain.vue
@@ -0,0 +1,86 @@
+<template>
+  <section class="content main">
+    <h2>Recent Updates:</h2>
+
+    <events-list :events="recentEvents" :is-recent="true"></events-list>
+
+    <div class="filter-container" style="margin-bottom: 25px">
+      <div class="search-input-field-name">
+        <input
+          type="text"
+          style="width: 200px;"
+          class="filter-item search-input"
+          placeholder="Search"
+          v-model="search"
+        >
+        <button class="search-icon">
+          <font-awesome-icon icon="search"/>
+        </button>
+      </div>
+      <a href="/create">
+        <button class="add_btn">
+          <font-awesome-icon class="icon" icon="calendar-plus"/>Add
+        </button>
+      </a>
+    </div>
+
+    <events-list :events="events" :fetchEvents="fetchEvents"></events-list>
+  </section>
+</template>
+
+<script>
+import EventsList from '../components/EventsList.vue'
+import axios from 'axios'
+
+export default {
+  name: 'ClientMain',
+  props: { clientId: String },
+  data() {
+    return {
+      currentSort: 'event_code',
+      currentSortDir: 'asc',
+      pageSize: 9,
+      currentPage: 1,
+      search: '',
+      events: [],
+      recentEvents: []
+    }
+  },
+  watch: {
+    clientId: function clientChanged() {
+      console.log('clientIdChanged')
+      this.fetchEvents()
+    }
+  },
+  methods: {
+    fetchEvents: function() {
+      const url =
+        'https://intempio-api-v3.herokuapp.com/api/v3/events/?clientID=' +
+        this.clientId
+
+      axios.get(url).then(response => {
+        this.events = response.data['records']
+        if (this.events === null) {
+          this.events = []
+          this.recentEvents = []
+          return
+        }
+        this.recentEvents = response.data['records'].filter((event, index) => {
+          if (
+            event.client_status == 'update' ||
+            event.client_status == 'urgent' ||
+            event.client_status == 'request'
+          )
+            return true
+        })
+      })
+    }
+  },
+  components: {
+    EventsList
+  },
+  mounted: function() {
+    this.fetchEvents()
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,71 +1,11 @@
 <template>
   <div id="app">
-    <div class="head">
-      <div class="header-admin">
-        <img src="~/assets/operation.png" class="mobile_btn">
-        <span>Operations Admin</span>
-      </div>
-      <div class="header-projects">Sunovion</div>
-    </div>
-    <div class="menu-wrap">
-      <template>
-        <Push noOverlay width="200">
-          <a id="Biogen" href="#">
-            <img src="~/assets/biogen.png">
-            <span>Biogen</span>
-          </a>
-          <a id="Sunovion" href="#">
-            <img src="~/assets/sunovion.png">
-            <span>Sunovion</span>
-          </a>
-          <a id="Unassigned" href="#">
-            <img src="~/assets/unassigned.png">
-            <span>Unassigned</span>
-          </a>
-        </Push>
-      </template>
-    </div>
-
-    <section class="main-content" id="page-wrap">
-      <div id="wrap">
-        <nuxt/>
-      </div>
-    </section>
+    <nuxt/>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
-import { Push } from 'vue-burger-menu'
-//import VueSidebarMenu from 'vue-sidebar-menu'
 
-export default {
-  components: {
-    Push // Register your component
-  }
-  /* data() {
-    menu: [
-      {
-        href: '/',
-        title: 'Dashboard',
-        icon: 'fa fa-user'
-      },
-      {
-        href: '#',
-        title: 'Charts',
-        icon: 'fa fa-chart-area'
-      }
-    ]
-  }  */
-  /* data() {
-    return {
-      items: [
-        { title: 'Home', icon: 'home', to: { name: 'index' } },
-        { title: 'Inspire', icon: 'lightbulb', to: { name: 'inspire' } },
-      ],
-    };
-  },*/
-}
-
-/*Vue.use(VueSidebarMenu) */
+export default {}
 </script>
