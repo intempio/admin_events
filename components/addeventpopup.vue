8496e86fc12a33049521b13a0dd610fab7cd6941
Kamil Polok <k.polok@inwebit.pl>
2019-08-05 21:44:52 +0200

    Fixed new event adding - loading tags

--- components/addeventpopup.vue
+++ components/addeventpopup.vue
@@ -2,6 +2,7 @@
   <div class="modal-mask" v-show="show" transition="modal">
     <div class="modal-wrapper">
       <div class="modal-container">
+
         <div class="modal-header">
           <slot name="header">
             <h2 class="colored">Products</h2>
@@ -10,16 +11,16 @@
 
         <div class="modal-body">
           <div class="radio-wrap">
-            <form>
-              <div v-for="(item, index) in Products" v-bind:key="item.product_id">
-                <b-radio
-                  v-model="selectedProduct[index]"
-                  name="product"
-                  :native-value="item.product_id">
+            <b-form-group>
+              <div v-for="item in Products" v-bind:key="item.product_id">
+                <b-form-radio
+                  @change="productSelect($event)"
+                  :value="item"
+                  name="product">
                   {{item.product_name}}
-                </b-radio>
+                </b-form-radio>
               </div>
-            </form>
+            </b-form-group>
           </div>
         </div>
 
@@ -29,6 +30,7 @@
             <button class="clone ml-1" @click="addEvent()">Next</button>
           </slot>
         </div>
+
       </div>
     </div>
   </div>
@@ -41,11 +43,11 @@
     name: 'addeventmodal',
     template: '#modal-addevent-template',
     props: {clientId: String},
-    data: function () {
+    data() {
       return {
         show: false,
         Products: [],
-        selectedProduct: []
+        selectedProduct: ''
       }
     },
     watch: {
@@ -60,37 +62,33 @@
       close: function () {
         this.show = false
       },
+      productSelect(product) {
+        this.selectedProduct = product.product_id;
+      },
       fetchProducts: function () {
         const url = '/api/v3/products/?clientID=' + this.clientId
         restService.get(url)
-          .then(response => this.Products = response.data)
+          .then(response => {
+            this.Products = response.data;
+          })
           .catch(err => {
             this.$toast.error(`Cannot fetch product list: ${err}`)
           })
       },
       addEvent: function () {
-        const url = process.env.VUE_APP_API + '/api/v3/events/'
-        var data = {
+        const url = process.env.VUE_APP_API + '/api/v3/events/';
+        const data = {
           client_id: this.clientId,
-          product_id: this.getProductId(this.selectedProduct)
-        }
-
+          product_id: this.selectedProduct
+        };
         restService
           .post(url, data)
           .then(response => {
             this.$router.push(`/admin/events/${response.data.event_id}`)
           })
           .catch(error => {
-            console.log(error);
             this.$toast.error(`Error: ${error}`);
           })
-      },
-
-      getProductId: function (selectedProduct) {
-        var i
-        for (i = 0; i < selectedProduct.length; i++) {
-          if (selectedProduct[i]) return selectedProduct[i]
-        }
       }
     },
     computed: {},
