ac26f2761d4ff2170e12f666c581905e50c48c77
unknown <marra@go2impact.com>
2020-06-09 18:49:44 +0800

    Update icons

--- pages/system-pick.vue
+++ pages/system-pick.vue
@@ -39,10 +39,12 @@
                 </router-link>
               </div>
               <div class="col-4">
-                <a href="https://intempio-scheduler.herokuapp.com/qa-events">QA</a>
+                <a href="https://intempio-scheduler.herokuapp.com/qa-events"><div class="tile">
+                    <i class="material-icons mb-3">fact_check</i><span>QA</span></div></a>
               </div>
 			  <div class="col-4">
-                <a href="https://intempio-scheduler.herokuapp.com/qa-events">Emails</a>
+                <a href="https://intempio-scheduler.herokuapp.com/qa-events"><div class="tile">
+                    <i class="material-icons mb-3">all_inbox</i><span>Emails</span></div></a>
               </div>
               <div class="col-4" v-if="visibleParts.includes('t_events')">
                 <router-link to="/tentative-events">
