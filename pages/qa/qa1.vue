057e8ab9a67a247fe3d368616b4f99e0d81ad7ed
marrajoy <marra@go2impact.com>
2020-08-14 20:33:34 +0800

    Removed items already in QA1 to Qa2

--- pages/qa/qa1.vue
+++ pages/qa/qa1.vue
@@ -1,114 +1,110 @@
 <template>
   <section>
     <div class="main">
-	    <clientheader :clientid="clientid ? clientid : ''" :sidebarOff="true" change-system="true"></clientheader>
-		<div class="container-fluid">
+      <clientheader
+        :clientid="clientid ? clientid : ''"
+        :sidebarOff="true"
+        change-system="true"
+      ></clientheader>
+      <div class="container-fluid">
         <div class="row mt-1 mb-3">
           <div class="col-xl-10 col-lg-12 m-auto">
-            <div class="go-back-button cursor-pointer" @click="goHome()">
+            <div
+              class="go-back-button cursor-pointer"
+              @click="goHome()"
+            >
               <i class="material-icons mr-2">chevron_left</i>
               <h6>Home</h6>
             </div>
           </div>
         </div>
       </div>
-	  <div class="container-fluid">
-        <div class="row mt-1 mb-3">
-          <div class="col-xl-10 col-lg-12 m-auto">
-      <div class="header-event">
-        <h3>Before Start QA</h3>
-        <table
-          id="event_details"
-          v-for="event_data in edata"
-          v-bind:key="event_data"
-        >
-          <tr>
-            <td><strong>Event Code:</strong> {{ event_data["event_code"] }}</td>
-            <td><strong>Event Name:</strong> {{ event_data["event_name"] }}</td>
-          </tr>
-        </table>
-      </div>
-      <table
-        border="1"
-        id="event_tags"
-        v-for="event_data in edata"
-        v-bind:key="event_data"
-      >
-        <tr>
-          <th>Tag Name</th>
-          <th>Value</th>
-          <th>Actions</th>
-        </tr>
-        <tr>
-          <td>Event Date Time</td>
-          <td>{{ event_data["event_start"] }}</td>
-          <td>Does date and time match the event request form from the Account Management folder, client email or calendar invite for this event?</td>
-        </tr>
-        <tr>
-          <td>Meeting Link</td>
-          <td>{{ event_data["cal_all Meeting Link"] }}</td>
-          <td>
-            - Check url and make sure it matches the one in client calendar
-          </td>
-        </tr>
-        <tr>
-          <t  d>Link Username/Password</td>
-          <td>{{ event_data["cal_all Meeting Username"] }} / {{ event_data["cal_all Meeting Password"] }}</td>
-          <td>- Meeting USERNAME is Intempio or (client)? <br />
-            - Check this link for reference:
-            <a href="
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            ://docs.google.com/spreadsheets/d/1k62mDH1pnn1PZTntgiNARZksbHzeTX7e5cO09tMQ1ZM/edit?ts=5a845fe2#gid=0">https://docs.google.com/spreadsheets/d/1k62mDH1pnn1PZTntgiNARZksbHzeTX7e5cO09tMQ1ZM/edit?ts=5a845fe2#gid=0</a>
-          </td>
-        </tr>
-        <tr>
-          <td>Producer Call Time</td>
-          <td>{{ event_data["producer_offset_minutes"] }}</td>
-          <td>Is Producer Call time correct? <br />
-            0 - Rehearsal/DryRun, Demo &amp; Techcheck <br />
-            60 - Remote <br />
-            120 - Onsite</td>
-        </tr>
-        <tr>
-          <td>Presenter Name</td>
-          <td>{{ event_data["cal_all Presenter 1 Name"] }}</td>
-          <td>
-            - Confirm Event Presenter. <br />
-            - Is presenter name existing?<br />
-            - Calendar with Speaker?<br />
-            - Calendar with Patient Speaker?<br />
-          </td>
-        </tr>
-        <tr>
-          <td>Client Contact</td>
-          <td>{{ event_data["Client Contact 1"] }}</td>
-          <td>
-            Is Client contact correct? <br />
-            Reference: <a href="https://docs.google.com/document/d/1UH8Yxr-tXpCkAaY9_Rq7Tk4uj35iWSyMwCwjA3viIH4/edit#">https://docs.google.com/document/d/1UH8Yxr-tXpCkAaY9_Rq7Tk4uj35iWSyMwCwjA3viIH4/edit#</a> <br />
-          </td>
-        </tr>
-      </table>
-      </div>
-    </div>
-	    </div>
       <div class="container-fluid">
         <div class="row mt-1 mb-3">
           <div class="col-xl-10 col-lg-12 m-auto">
-      <div class="done">
-        <button @click="done">Done</button>
+            <div class="header-event">
+              <h3>Before Start QA</h3>
+              <table
+                id="event_details"
+                v-for="event_data in edata"
+                v-bind:key="event_data"
+              >
+                <tr>
+                  <td><strong>Event Code:</strong> {{ event_data["event_code"] }}</td>
+                  <td><strong>Event Name:</strong> {{ event_data["event_name"] }}</td>
+                </tr>
+              </table>
+            </div>
+            <table
+              border="1"
+              id="event_tags"
+              v-for="event_data in edata"
+              v-bind:key="event_data"
+            >
+              <tr>
+                <th>Tag Name</th>
+                <th>Value</th>
+                <th>Actions</th>
+              </tr>
+              <tr>
+                <td>Event Date Time</td>
+                <td>{{ event_data["event_start"] }}</td>
+                <td>Does date and time match the event request form from the Account Management folder, client email or calendar invite for this event?</td>
+              </tr>
+              <tr>
+                <td>Meeting Link</td>
+                <td>{{ event_data["cal_all Meeting Link"] }}</td>
+                <td>
+                  - Check url and make sure it matches the one in client calendar
+                </td>
+              </tr>
+              <tr>
+                <td>Link Username/Password</td>
+                <td>{{ event_data["cal_all Meeting Username"] }} / {{ event_data["cal_all Meeting Password"] }}</td>
+                <td>- Meeting USERNAME is Intempio or (client)? <br />
+                  - Check this link for reference:
+                  <a href="https://docs.google.com/spreadsheets/u/6/d/1k62mDH1pnn1PZTntgiNARZksbHzeTX7e5cO09tMQ1ZM/edit?ts=5a845fe2#gid=0">https://docs.google.com/spreadsheets/d/1k62mDH1pnn1PZTntgiNARZksbHzeTX7e5cO09tMQ1ZM/edit?ts=5a845fe2#gid=0</a>
+                </td>
+              </tr>
+              <tr>
+                <td>Producer Call Time</td>
+                <td>{{ event_data["producer_offset_minutes"] }}</td>
+                <td>Is Producer Call time correct? <br />
+                  0 - Rehearsal/DryRun, Demo &amp; Techcheck <br />
+                  60 - Remote <br />
+                  120 - Onsite</td>
+              </tr>
+              <tr>
+                <td>Presenter Name</td>
+                <td>{{ event_data["cal_all Presenter 1 Name"] }}</td>
+                <td>
+                  - Confirm Event Presenter. <br />
+                  - Is presenter name existing?<br />
+                  - Calendar with Speaker?<br />
+                  - Calendar with Patient Speaker?<br />
+                </td>
+              </tr>
+              <tr>
+                <td>Client Contact</td>
+                <td>{{ event_data["Client Contact 1"] }}</td>
+                <td>
+                  Is Client contact correct? <br />
+                  Reference: <a href="https://docs.google.com/document/d/1UH8Yxr-tXpCkAaY9_Rq7Tk4uj35iWSyMwCwjA3viIH4/edit#">https://docs.google.com/document/d/1UH8Yxr-tXpCkAaY9_Rq7Tk4uj35iWSyMwCwjA3viIH4/edit#</a> <br />
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
       </div>
+      <div class="container-fluid">
+        <div class="row mt-1 mb-3">
+          <div class="col-xl-10 col-lg-12 m-auto">
+            <div class="done">
+              <button @click="done">Done</button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
     </div>
   </section>
 </template>
@@ -151,7 +147,7 @@ components: {clientheader},
         .catch(error => {
           this.$toast.error(`Error: ${error}`)
         });
-          
+
     },
     done: function() {
       this.eventID = this.$route.query.eventID;
--- pages/qa/qa2.vue
+++ pages/qa/qa2.vue
@@ -1,162 +1,131 @@
 <template>
   <section>
     <div class="main">
-	    <clientheader :clientid="clientid ? clientid : ''" :sidebarOff="true" change-system="true"></clientheader>
-		<div class="container-fluid">
+      <clientheader
+        :clientid="clientid ? clientid : ''"
+        :sidebarOff="true"
+        change-system="true"
+      ></clientheader>
+      <div class="container-fluid">
         <div class="row mt-1 mb-3">
           <div class="col-xl-10 col-lg-12 m-auto">
-            <div class="go-back-button cursor-pointer" @click="goHome()">
+            <div
+              class="go-back-button cursor-pointer"
+              @click="goHome()"
+            >
               <i class="material-icons mr-2">chevron_left</i>
               <h6>Home</h6>
             </div>
           </div>
         </div>
       </div>
-	  <div class="container-fluid">
-        <div class="row mt-1 mb-3">
-          <div class="col-xl-10 col-lg-12 m-auto">
-      <div class="header-event">
-        <h3>Before Start QA</h3>
-        <table
-          id="event_details"
-          v-for="event_data in edata"
-          v-bind:key="event_data"
-        >
-          <tr>
-            <td><strong>Event Code:</strong> {{ event_data["event_code"] }}</td>
-            <td><strong>Event Name:</strong> {{ event_data["event_name"] }}</td>
-          </tr>
-        </table>
-      </div>
-      <table
-        border="1"
-        id="event_tags"
-        v-for="event_data in edata"
-        v-bind:key="event_data"
-      >
-        <tr>
-          <th>Tag Name</th>
-          <th>Value</th>
-          <th>Actions</th>
-        </tr>
-        <tr>
-          <td>Event Date Time</td>
-          <td>{{ event_data["event_start"] }}</td>
-          <td>Does date and time match the event request form from the Account Management folder, client email or calendar invite for this event?</td>
-        </tr>
-        <tr>
-          <td>Producer Call Time</td>
-          <td>{{ event_data["producer_offset_minutes"] }}</td>
-          <td>Is the producer call time correct?</td>
-        </tr>
-        <tr>
-          <td>Room Type</td>
-          <td>{{ event_data["cal_producer Room Type"] }}</td>
-          <td>- Meeting Room: 0-99 <br />
-            - Seminar Room: 100-1000
-            </td>
-        </tr>
-        <tr>
-          <td>Meeting Link Owner</td>
-          <td>{{ event_data["cal_producer Meeting Link Owner"] }}</td>
-          <td>1. Is meeting link owned by Client or Intempio? <br />
-2. Is meeting link from Adobe Connect or Zoom?</td>
-        </tr>
-        <tr>
-          <td>Meeting Link</td>
-          <td>{{ event_data["cal_all Meeting Link"] }}</td>
-          <td>
-           1. Is Meeting link same as link from runsheet, Internal Calendar Invite and/or Client Invite (if any)? <br />
-2. Is Meeting link correct?
-          </td>
-        </tr>
-        <tr>
-          <td>Meeting Username</td>
-          <td>{{ event_data["cal_all Meeting Username"] }}</td>
-          <td>
-            1. Are the Proper AC Host Credentials Assigned? ( NOTE: Seminar Room condition & Client Link condition)
-            </td>
-        </tr>
-        <tr>
-          <td>Meeting Password</td>
-          <td>{{ event_data["cal_all Meeting Password"] }}</td>
-          <td>
-          2. Enter AC using assigned Host Credentials. Did you enter as Host? </td>
-        </tr>
-        <tr>
-          <td>Recording</td>
-          <td>{{ event_data["Recording"] }}</td>
-          <td>
-            Does the event need to be recorded?</td>
-        </tr>
-        <tr>
-          <td>Audio License</td>
-          <td>{{ event_data["cal_producer Audio License"] }}</td>
-          <td>
-            1. Is audio owned by Client or Intempio? <br/>
-2. Is audio from Adobe Connect or Zoom? </td>
-        </tr>
-        <tr>
-          <td>Audio Name</td>
-          <td>{{ event_data["cal_producer Audio Name"] }}</td>
-          <td>
-            Adobe Connect <br/>
-1. Is the audio profile used simultaneously on other events? <br/>
-2. Is the TC, Host, Participant code correct per the AC Host Code Map <br/>
-3. Does the audio information match data from the runsheet, Internal Calendar Invite and/or Client Invite (if any)? <br/>
-4. Is the audio configured correctly? <br/>
-Zoom - Does the Audio Information match the data inside zoom meeting? </td>
-        </tr>
-        <tr>
-          <td>Audio Type</td>
-          <td>{{ event_data["cal_producer Audio Type"] }}</td>
-          <td>
-            For correct Audio type, refer to this link: t.ly/rx070</td>
-        </tr>
-        <tr>
-          <td>Event Document</td>
-          <td>{{ event_data["cal_all Event Documents"] }}</td>
-          <td>
-            1. Is the Link directing to the correct Event Document Folder? <br/>
-(Event name should be the same as the event document name) <br/>
-2. Is the event document sharing settings set to "anyone with the link ... can edit"? <br/>
-3. Does the event document have a runsheet  in it?
-           </td>
-        </tr>
-        <tr>
-          <td>Breakouts</td>
-          <td>{{ event_data["Breakouts"] }}</td>
-          <td>- Is there breakouts? <br/>
-Refer to Internal or Producer notes in Cribs
-            </td>
-        </tr>
-        <tr>
-          <td>Client Contact</td>
-          <td>{{ event_data["Client Contact 1"] }}</td>
-          <td>- Is the client contact correct? <br/>
-Refer to emails received on this event - Client contact is the person who requested for the event.
-           </td>
-        </tr>
-        <tr>
-          <td>Internal Notes</td>
-          <td>{{ event_data["internal_notes"] }}</td>
-          <td>Do Internal notes have any update on the event? <br/>
-Are tags accurate per updates on the internal notes?
-           </td>
-        </tr>
-      </table>
-      </div>
-    </div>
-	    </div>
       <div class="container-fluid">
         <div class="row mt-1 mb-3">
           <div class="col-xl-10 col-lg-12 m-auto">
-      <div class="done">
-        <button @click="done">Done</button>
+            <div class="header-event">
+              <h3>Before Start QA</h3>
+              <table
+                id="event_details"
+                v-for="event_data in edata"
+                v-bind:key="event_data"
+              >
+                <tr>
+                  <td><strong>Event Code:</strong> {{ event_data["event_code"] }}</td>
+                  <td><strong>Event Name:</strong> {{ event_data["event_name"] }}</td>
+                </tr>
+              </table>
+            </div>
+            <table
+              border="1"
+              id="event_tags"
+              v-for="event_data in edata"
+              v-bind:key="event_data"
+            >
+              <tr>
+                <th>Tag Name</th>
+                <th>Value</th>
+                <th>Actions</th>
+              </tr>
+              <tr>
+                <td>Room Type</td>
+                <td>{{ event_data["cal_producer Room Type"] }}</td>
+                <td>- Meeting Room: 0-99 <br />
+                  - Seminar Room: 100-1000
+                </td>
+              </tr>
+              <tr>
+                <td>Meeting Link Owner</td>
+                <td>{{ event_data["cal_producer Meeting Link Owner"] }}</td>
+                <td>1. Is meeting link owned by Client or Intempio? <br />
+                  2. Is meeting link from Adobe Connect or Zoom?</td>
+              </tr>
+              <tr>
+                <td>Recording</td>
+                <td>{{ event_data["Recording"] }}</td>
+                <td>
+                  Does the event need to be recorded?</td>
+              </tr>
+              <tr>
+                <td>Audio License</td>
+                <td>{{ event_data["cal_producer Audio License"] }}</td>
+                <td>
+                  1. Is audio owned by Client or Intempio? <br />
+                  2. Is audio from Adobe Connect or Zoom? </td>
+              </tr>
+              <tr>
+                <td>Audio Name</td>
+                <td>{{ event_data["cal_producer Audio Name"] }}</td>
+                <td>
+                  Adobe Connect <br />
+                  1. Is the audio profile used simultaneously on other events? <br />
+                  2. Is the TC, Host, Participant code correct per the AC Host Code Map <br />
+                  3. Does the audio information match data from the runsheet, Internal Calendar Invite and/or Client Invite (if any)? <br />
+                  4. Is the audio configured correctly? <br />
+                  Zoom - Does the Audio Information match the data inside zoom meeting? </td>
+              </tr>
+              <tr>
+                <td>Audio Type</td>
+                <td>{{ event_data["cal_producer Audio Type"] }}</td>
+                <td>
+                  For correct Audio type, refer to this link: t.ly/rx070</td>
+              </tr>
+              <tr>
+                <td>Event Document</td>
+                <td>{{ event_data["cal_all Event Documents"] }}</td>
+                <td>
+                  1. Is the Link directing to the correct Event Document Folder? <br />
+                  (Event name should be the same as the event document name) <br />
+                  2. Is the event document sharing settings set to "anyone with the link ... can edit"? <br />
+                  3. Does the event document have a runsheet in it?
+                </td>
+              </tr>
+              <tr>
+                <td>Breakouts</td>
+                <td>{{ event_data["Breakouts"] }}</td>
+                <td>- Is there breakouts? <br />
+                  Refer to Internal or Producer notes in Cribs
+                </td>
+              </tr>
+              <tr>
+                <td>Internal Notes</td>
+                <td>{{ event_data["internal_notes"] }}</td>
+                <td>Do Internal notes have any update on the event? <br />
+                  Are tags accurate per updates on the internal notes?
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
       </div>
+      <div class="container-fluid">
+        <div class="row mt-1 mb-3">
+          <div class="col-xl-10 col-lg-12 m-auto">
+            <div class="done">
+              <button @click="done">Done</button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
     </div>
   </section>
 </template>
@@ -201,7 +170,7 @@ components: {clientheader},
         .catch(error => {
           this.$toast.error(`Error: ${error}`)
         });
-          
+
     },
     done: function() {
       this.eventID = this.$route.query.eventID;
